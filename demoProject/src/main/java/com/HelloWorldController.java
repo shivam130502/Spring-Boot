817a8f69f78946d95c8f4ea1105ac7354b82e911
Shivam Dhir <shivamdhir90@gmail.com>
2024-07-23 23:46:53 -0700

    added new class HelloWorldController

--- demoProject/src/main/java/com/HelloWorldController.java
+++ demoProject/src/main/java/com/HelloWorldController.java
@@ -1,5 +1,17 @@
 package com;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.Generated; 
+
+@RestController 
 public class HelloWorldController {
 
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
 }
+
