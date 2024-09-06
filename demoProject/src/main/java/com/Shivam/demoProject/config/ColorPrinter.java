3e892a8c36ba3ecc8a2fce6acf31819487180fe4
Shivam Dhir <shivamdhir90@gmail.com>
2024-09-06 11:32:48 -0700

    added config file

--- demoProject/src/main/java/com/Shivam/demoProject/config/ColorPrinter.java
+++ demoProject/src/main/java/com/Shivam/demoProject/config/ColorPrinter.java
@@ -2,9 +2,13 @@ package com.Shivam.demoProject.config;
 
 public class ColorPrinter {
 
-        String color;
+    String color;
 
     public ColorPrinter(String color){
         this.color = "returns color";
+    }
+
+    private String currentColor(){
+        return this.color;
     }
 }
